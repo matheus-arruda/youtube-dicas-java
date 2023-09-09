89bb52a849c5dd0b69ea434a64cfa74e5cdd35f4
Matheus Arruda <matheush13@gmail.com>
2023-09-09 15:12:05 -0300

    Projeto inicial - Aula 1

---
+++ src/main/java/com/matheus/dicasjava/DicasJavaApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.dicasjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicasJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicasJavaApplication.class, args);
+	}
+
+}
